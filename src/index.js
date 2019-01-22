fd7a373149bd3932a5c01441d1776e71900a65fd
Valentino Gagliardi <valentino@valentinog.com>
2019-01-22 13:42:21 +0100

    Connecting React with Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// if you're in create-react-app import the files as:
+// import store from "./js/store/index";
+// import App from "./js/components/App.jsx";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  // The target element might be either root or app,
+  // depending on your development environment
+  // document.getElementById("app")
+  document.getElementById("root")
+);
---
+++ src/js/components/List.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = state => {
+    return { articles: state.articles };
+};
+
+const ConnectedList = ({ articles }) => (
+    <ul className="list-group list-group-flush">
+        {articles.map(el => (
+            <li className="list-group-item" key={el.id}>
+                {el.title}
+            </li>
+        ))}
+    </ul>
+);
+
+const List = connect(mapStateToProps)(ConnectedList);
+
+export default List;
