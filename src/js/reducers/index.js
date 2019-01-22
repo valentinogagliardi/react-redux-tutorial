1f5790068c871a3e35629ab582403726422b6c43
Valentino Gagliardi <valentino@valentinog.com>
2019-01-22 13:30:32 +0100

    Refactoring the reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,16 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
   articles: []
 };
+
 function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
   return state;
 }
+
 export default rootReducer;
